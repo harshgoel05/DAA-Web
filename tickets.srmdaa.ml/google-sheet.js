a0aa834a8513b5e9bcc8295d8bdfcf8b984c45a2
harshgoel05 <harshgoel05@gmail.com>
2019-10-15 10:41:13 +0530

    if19

--- tickets.srmdaa.ml/google-sheet.js
+++ tickets.srmdaa.ml/google-sheet.js
@@ -7,33 +7,18 @@ $("#dataform").submit(function(event){
     var $form = $(this);
     var $inputs = $form.find("input, select, button, textarea");
     var serializedData = $form.serialize();
-    // $inputs.prop("disabled", true);
-
-    // Fire off the request to /form.php
     request = $.ajax({
         url: "https://script.google.com/macros/s/AKfycbwsO0ph98mIC32fX3Q1uWSXn0Y3qR80dzbi4sCTJd1ZXc_ra6HB/exec",
+        // url: "https://script.google.com/macros/s/AKfycbxbSlf1F3teZDihR2QIuGBKWELIZ4APk8sEE5Vg7L-Mq8VM06e6/exec",
         type: "post",
         data: serializedData
     });
-
-    // Callback handler that will be called on success
-    request.done(function (response, textStatus, jqXHR){
-        // Log a message to the console
-        // console.log("Hooray, it worked!");
-        // console.log(response);
-        // console.log(textStatus);
-        // console.log(jqXHR);
-        
+    request.done(function (response, textStatus, jqXHR){      
     window.open('registered.html','_self',false);
     });
 
-    // Callback handler that will be called on failure
     request.fail(function (jqXHR, textStatus, errorThrown){
-        // Log the error to the console
-        console.error(
-            "The following error occurred: "+
-            textStatus, errorThrown
-        );
+        document.getElementById("demo1").style.display = "none";
     });
     event.preventDefault();
 });
